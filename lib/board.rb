663e177d2ce7b8b5ff419b555d862e32c2553fd9
Brandon Calloway <brandonvcalloway@gmail.com>
2019-12-02 16:09:25 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,25 @@
-# Define the variable board below.
+row = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+row[0]
+
+row[1]
+
+row[2]
+
+row[1]="X"
+
+row[3]
+
+row[4]
+
+row[5]
+
+row[4] ="X"
+
+row[6]
+
+row[7]
+
+row[8]
+
+row[7] ="X"
