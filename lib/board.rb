4391978d1947555b1fca628e6eeb302f5e7c3669
Brandon Calloway <brandonvcalloway@gmail.com>
2019-12-02 16:19:54 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,14 @@
 row = [" ", " ", " ", " ", " "," ", " "," "," ",]
-# Position 1 left
+
+# Position 1: left
+row[0]
+
+# Position 2: Middle
+row[1]
+
+# Position 3: Right
+row[2]
+
+# Move X to the middle Position 2, middle
+row[1] = "X"
+#=>
